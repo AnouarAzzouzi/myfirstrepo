c85cb8cf831bd429ec129938826908edb66bf6f8
AnouarAzzouzi <175862@student.horizoncollege.nl>
2024-06-19 02:57:55 +0200

    Update sendmail nieuw.ps1

--- sendmail nieuw.ps1
+++ sendmail nieuw.ps1
@@ -60,33 +60,4 @@ Send-MailMessage -BodyAsHtml $htmlContent -To $to -Subject $subject -SmtpServer 
 
 Write-Output "E-mail is verstuurd."
 
-# Certificaat aanmaken en signeren
-$cert = New-SelfSignedCertificate -DnsName "MAXTERMUX" -CertStoreLocation "Cert:\LocalMachine\My"
-Set-AuthenticodeSignature -FilePath "C:\Users\ishel\OneDrive\Documenten\send-mail" -Certificate $cert
 
-# SIG # Begin signature block
-# MIIFuQYJKoZIhvcNAQcCoIIFqjCCBaYCAQExDzANBglghkgBZQMEAgEFADB5Bgor
-# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
-# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCDXWMvZ4XfWjKp3
-# M++dgz/7DzmeemA6PfaDhyl+/qaDkKCCAyIwggMeMIICBqADAgECAhAw8+WQSyql
-# gEOwHTojBbHwMA0GCSqGSIb3DQEBCwUAMCcxJTAjBgNVBAMMHFBvd2VyU2hlbGwg
-# Q29kZSBTaWduaW5nIENlcnQwHhcNMjQwNjA1MDc1MjMyWhcNMjUwNjA1MDgxMjMy
-# WjAnMSUwIwYDVQQDDBxQb3dlclNoZWxsIENvZGUgU2lnbmluZyBDZXJ0MIIBIjAN
-# BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxb4mMEuFaETJ68IXfSInwdxmuWDc
-# nlUGYb/Ihhgg9bA6lY0+tGAp/3qgJy0frcAsxbGs50HlWdxZUjQ1ltjVWRHYD+X0
-# 5Cl0tP4XGLJYNi/23LdhFX+95OUYI5RxCkhUoA9a3L9AaPTzbVEABS4sj0hC7W+v
-# joPkGnbvZemo1eHEFybYgmHlAqFlSXSUJIRsIJwEw9Vzo8PM/mWEaHHCixxrHP5u
-# OXz8V1F9lM4swJofI7JdEtX9SEpHIkTMTj8F8+tK4xlM2kxpYXxPY7erKznDRiLQ
-# OhyANpUu1tD2kpDrGu9vFBJce31J2VTeXkmGcGx8vq9YR/F3qJsAZ0X7cQIDAQAB
-# o0YwRDAOBgNVHQ8BAf8EBAMCB4AwEwYDVR0lBAwwCgYIKwYBBQUHAwMwHQYDVR0O
-# BBYEFKx+z+nPpuFaumrilpB8inTzrweYMA0GCSqGSIb3DQEBCwUAA4IBAQClZo5M
-# ukgrVq6GLoctaO6qIHPn5CxnGXWeOHqYd8ut2DpdM9azO/jobPrByZ7UQ7RWkom+
-# p3zXlr/bbJ1fc9zvzlkMaJLzEQyxdzNOYV8SkItqSk33O/z6rAjFD3g5Z98zieMN
-# FcdOmckRq+eayhgu3sDoQONRbZw5KwkLP7dGlGehRT8Ep/97h8xs7+D1cY/oFYBX
-# nTNAs6Nv3PyvRYOMRRatAWMfQdHHvZ7AlJfyjXe/hehn8J/1Ju9/CaNAmMizUtoe
-# O7U2S5AWSVRoMXwwU0aExZZ/PaC+wpJinmQVosEYoDMnSq25JV1ElTpsfTVyGJol
-# GsCKARiJmnznIkhmMYIB7TCCAekCAQEwOzAnMSUwIwYDVQQDDBxQb3dlclNoZWxs
-# IENvZGUgU2lnbmluZyBDZXJ0AhAw8+WQSyqlgEOwHTojBbHwMA0GCWCGSAFlAwQC
-# AQUAoIGEMBgGCisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwG
-# CisGAQQBgjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwLwYJKoZI
-# hvcNAQkEMSIE
